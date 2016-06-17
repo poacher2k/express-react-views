72ea18abf073a814abe48070200509697bfb1507
Daniel Skogly <daniel.skogly@gmail.com>
2016-06-17 13:51:34 +0200

    Use renderToString instead of static

--- index.js
+++ index.js
@@ -14,69 +14,67 @@ var assign = require('object-assign');
 var _escaperegexp = require('lodash.escaperegexp');
 
 var DEFAULT_OPTIONS = {
-  doctype: '<!DOCTYPE html>',
-  beautify: false,
-  transformViews: true,
-  babel: {
-    presets: [
-      'react',
-      'es2015',
-    ],
-  },
+	doctype: '<!DOCTYPE html>',
+	beautify: false,
+	transformViews: true,
+	babel: {
+		presets: ['react', 'es2015',]
+	}
 };
 
 function createEngine(engineOptions) {
-  var registered = false;
-  var moduleDetectRegEx;
+	var registered = false;
+	var moduleDetectRegEx;
 
-  engineOptions = assign({}, DEFAULT_OPTIONS, engineOptions || {});
+	engineOptions = assign({}, DEFAULT_OPTIONS, engineOptions || {});
 
-  function renderFile(filename, options, cb) {
-    // Defer babel registration until the first request so we can grab the view path.
-    if (!moduleDetectRegEx) {
-      // Path could contain regexp characters so escape it first.
-      moduleDetectRegEx = new RegExp('^' + _escaperegexp(options.settings.views));
-    }
-    if (engineOptions.transformViews && !registered) {
-      // Passing a RegExp to Babel results in an issue on Windows so we'll just
-      // pass the view path.
-      require('babel-register')(
-        assign({only: options.settings.views}, engineOptions.babel)
-      );
-      registered = true;
-    }
+	function renderFile(filename, options, cb) {
+		// Defer babel registration until the first request so we can grab the view path.
+		if (!moduleDetectRegEx) {
+			// Path could contain regexp characters so escape it first.
+			moduleDetectRegEx = new RegExp('^' + _escaperegexp(options.settings.views));
+		}
+		if (engineOptions.transformViews && !registered) {
+			// Passing a RegExp to Babel results in an issue on Windows so we'll just
+			// pass the view path.
+			require('babel-register')(assign({
+				only: options.settings.views
+			}, engineOptions.babel));
+			registered = true;
+		}
 
-    try {
-      var markup = engineOptions.doctype;
-      var component = require(filename);
-      // Transpiled ES6 may export components as { default: Component }
-      component = component.default || component;
-      markup += ReactDOMServer.renderToStaticMarkup(
-        React.createElement(component, options)
-      );
-    } catch (e) {
-      return cb(e);
-    } finally {
-      if (options.settings.env === 'development') {
-        // Remove all files from the module cache that are in the view folder.
-        Object.keys(require.cache).forEach(function(module) {
-          if (moduleDetectRegEx.test(require.cache[module].filename)) {
-            delete require.cache[module];
-          }
-        });
-      }
-    }
+		try {
+			var markup = engineOptions.doctype;
+			var componentParts = require(filename);
+			// Transpiled ES6 may export components as { default: Component }
+			componentParts = componentParts.default || componentParts;
+			markup += componentParts.pre(options);
+			//   markup += ReactDOMServer.renderToStaticMarkup(
+			markup += ReactDOMServer.renderToString(React.createElement(componentParts.main, options));
+			markup += componentParts.post(options);
+		} catch (e) {
+			return cb(e);
+		} finally {
+			if (options.settings.env === 'development') {
+				// Remove all files from the module cache that are in the view folder.
+				Object.keys(require.cache).forEach(function(module) {
+					if (moduleDetectRegEx.test(require.cache[module].filename)) {
+						delete require.cache[module];
+					}
+				});
+			}
+		}
 
-    if (engineOptions.beautify) {
-      // NOTE: This will screw up some things where whitespace is important, and be
-      // subtly different than prod.
-      markup = beautifyHTML(markup);
-    }
+		if (engineOptions.beautify) {
+			// NOTE: This will screw up some things where whitespace is important, and be
+			// subtly different than prod.
+			markup = beautifyHTML(markup);
+		}
 
-    cb(null, markup);
-  }
+		cb(null, markup);
+	}
 
-  return renderFile;
+	return renderFile;
 }
 
 exports.createEngine = createEngine;
